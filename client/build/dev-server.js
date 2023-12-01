c10d16bc0b86f98027a05d2d568b3a2062545df1
Sudhin <sudhin.acharya@mendix.com>
2023-12-01 16:03:53 +0100

    commit package json files

--- client/build/dev-server.js
+++ client/build/dev-server.js
@@ -85,7 +85,7 @@ devMiddleware.waitUntilValid(() => {
   // when env is testing, don't need open it
   if (autoOpenBrowser && process.env.NODE_ENV !== 'testing') {
     // opn(uri)
-    spawn('open', [uri]);
+    //spawn('open', [uri]);
   }
   _resolve()
 })
